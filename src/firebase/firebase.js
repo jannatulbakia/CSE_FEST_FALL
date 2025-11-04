03ef8183a67f7d68695825c339b1df20cec07369
bakia <bakia.nimmy@gmail.com>
2025-11-04 11:49:47 +0600

    updated

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
+// Import the functions you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyC6LWV_aaR8k4bN2dv_b58q345aI_sxgm8",
-  authDomain: "smart-health-de98f.firebaseapp.com",
-  projectId: "smart-health-de98f",
-  storageBucket: "smart-health-de98f.firebasestorage.app",
-  messagingSenderId: "979797755671",
-  appId: "1:979797755671:web:1c69b79e34523ae1b3d952",
-  measurementId: "G-ZBHLZMWE11"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
